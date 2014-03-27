30929bbc76061bf1cebc82072dbfac8b6be5d4e5
Yonghwan Lee <thetaiz@me.com>
2014-03-27 20:08:27 +0900

    1. 로그인 폼 수정 및 CSS 추가 2. 로그인 처리 추가 3. 회원 가입 폼 수정 및 CSS 추가 4. 회원 가입 처리 수정

---
+++ server/apps/account/controllers/AccountController.js
@@ -0,0 +1,42 @@
+var passport = require('passport'),
+    Account = require('../Account'),
+    Tokenizer = require('../../verification/Tokenizer.js');
+
+function onSignUp (req, res) {
+    var username = req.body.username,
+        email = req.body.email,
+        password = req.body.password;
+
+    var account = new Account({
+        username: username,
+        email: email
+    });
+
+    Account.register(account, password, function (err, account) {
+        if (err) {
+            console.log(err);
+            res.render('formSignUp', {account: account});
+        }
+
+        var authByToken = Tokenizer(req, res);
+
+        authByToken.createToken(account, function(err, token) {
+            if (err) throw err;
+
+            console.log(token);
+            authByToken.sendVerificationEmail(account, token);
+
+            passport.authenticate('local')(req, res, function () {
+                res.send('Send verification email by token \''+ token +'\'. Complete!');
+            });
+        });
+    });
+}
+
+function onSignIn (req, res) {
+    // Todo: 로그인 과정을 마치면 사용자 정보를 홈으로 전달
+    res.redirect('/');
+}
+
+exports.signup = onSignUp;
+exports.signin = onSignIn;
--- server/apps/account/index.js
+++ server/apps/account/index.js
@@ -1,20 +1,25 @@
-/**
- * 로그인/로그아웃/회원가입 담당 페이지
- */
-var express		= require('express');
-var app			= module.exports = express();
-var controller	= require('./controllers/RegisterController');
+var express = require('express'),
+    app = module.exports = express(),
+    passport = require('passport'),
+    controller = require('./controllers/AccountController');
 
-app.set('views', __dirname + '/views' );
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-
-app.get('/login', function (req, res) {
-	res.render('loginForm');
+// Show the login form
+app.get('/signin', function (req, res) {
+    res.render('formSignIn');
 });
 
-app.get('/register', function (req, res) {
-	res.render('registerForm')
+// Process the login form
+app.post('/signin', passport.authenticate('local', {
+    failureRedirect: '/signin'
+}), controller.signin);
+
+// Show the sign up form
+app.get('/signup', function (req, res) {
+    res.render('formSignUp');
 });
 
-app.post('/register', controller.register);
+// Process the sign up form
+app.post('/signup', controller.signup);

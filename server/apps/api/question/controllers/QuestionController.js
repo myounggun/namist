966bb5991a6d42e9bb9db343dca3b12b6bcfc186
myounggun <myounggun@gmail.com>
2014-03-28 17:54:19 +0900

    문제 조회 API에 페이징 추가.

--- server/apps/api/question/controllers/QuestionController.js
+++ server/apps/api/question/controllers/QuestionController.js
@@ -4,23 +4,85 @@
 var Question = require('../../../admin/question/Question');
 var async = require('async');
 
-exports.list = list;
-exports.read = read;
-
 /**
- * 문제 목록 API
+ * 문제 출제 목록 API (query: page, pageSize)
  */
 function list(req, res) {
-	Question.find({}, {}, {sort : {id: -1}}, function (err, docs) {	
-		res.send(docs);
-	});
+	var page     = parseInt(req.query.page, 10);
+	var pageSize = parseInt(req.query.pageSize, 10) || 3;
+	
+	var pagingOption = {};
+	
+	if (page) {
+		pagingOption = {
+				sort  : {id: -1},
+				skip  : (page - 1) * pageSize,
+				limit : pageSize
+		};
+	}
+
+	async.waterfall([
+   		function(callback) {
+   			Question.count({}, function(err, count) {
+   				callback(null, count);
+   			});
+   		},
+   		function(totalCount, callback) {
+   			
+   			Question.find({}, {}, pagingOption, function (err, docs) {
+   				var result = {
+   						page      : page,
+   						pageSize  : pageSize,
+   						totalPage : Math.ceil(totalCount / pageSize),
+   						items     : refineDocs(docs, page, pageSize)
+   				};
+   				
+   				res.json(result);
+ 			});
+   		}
+   	], 
+   	function (err, result) {
+ 		if (err) throw err;
+   	});
 }
 
 /**
- * 문제 정보 API
+ * 문제 출제 정보 API (query: id)
  */
 function read(req, res) {
-	Question.find({id: req.query.id}, function(err, docs) {
-		res.send(docs);
+	Question.findOne({
+		id: req.query.id
+	}, 
+	function(err, doc) {
+		res.json(refineDoc(doc));
 	});
 }
+
+function refineDocs(docs) {
+	if (!docs) {
+		return "No Data";
+	}
+
+	var result = [];
+	for (var i = 0, l = docs.length; i < l; i++) {
+		var doc = docs[i];
+		result.push(refineDoc(doc));
+	}
+	
+	return result;
+}
+
+function refineDoc(doc) {
+	return {
+		id      : doc.id,
+		date    : doc.date,
+		image   : doc.image,
+		time    : doc.time,
+		titles  : doc.title,
+		
+		displayDate : doc.displayDate // virtual
+	}
+}
+
+exports.list = list;
+exports.read = read;

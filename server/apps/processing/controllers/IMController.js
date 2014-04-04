790fa87860c6d5041c29d0cbbe3fbe9e727dd28b
myounggun <myounggun@gmail.com>
2014-04-04 16:49:22 +0900

    imagemagick 테스트 추가.

--- server/apps/processing/controllers/IMController.js
+++ server/apps/processing/controllers/IMController.js
@@ -7,25 +7,18 @@ var path = require('path');
 var imagePath = path.join(__dirname, '../../../../client/images/sw.jpg');
 
 function test(req, res) {
-	res.render('index', {
-		locals: {
-			title : 'image processing',
-			imagePath : imagePath
-		},
-		cache: false
-	});
-	
-	return;
-
 	im.identify(imagePath, function(err, features){
 	  if (err) throw err;
-	  console.log(features);
-	  // { format: 'JPEG', width: 3904, height: 2622, depth: 8 }
-	});
-	
-	easyimg.info(imagePath, function(err, stdout, stderr) {
-	  if (err) throw err;
-	  console.log(stdout);
+	  
+	  	res.json(features);
+//		res.render('index', {
+//			locals: {
+//				title     : 'image processing',
+//				imagePath : imagePath,
+//				features  : features // { format: 'JPEG', width: 3904, height: 2622, depth: 8 }
+//			},
+//			cache: false
+//		});
 	});
 }
 

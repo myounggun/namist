838f4a386e343ac7f037adbcb4632ab5ca154454
myounggun <myounggun@gmail.com>
2014-04-28 16:25:40 +0900

    문제 등록/수정 시 multipartMiddleware 적용 (connect-multiparty 모듈 사용).

--- server/apps/admin/question/index.js
+++ server/apps/admin/question/index.js
@@ -3,6 +3,8 @@
  */
 var express = require('express');
 var app = module.exports = express();
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 var controller = require('./controllers/QuestionController');
 
@@ -13,7 +15,7 @@ app.get('/admin/question', controller.list);
 app.get('/admin/question/add', controller.add);
 app.get('/admin/question/edit/:id', controller.edit);
 
-app.post('/admin/question/create', controller.create);
+app.post('/admin/question/create', multipartMiddleware, controller.create);
 app.get('/admin/question/:id', controller.read);
-app.put('/admin/question', controller.update);
+app.put('/admin/question', multipartMiddleware, controller.update);
 app.del('/admin/question', controller.del);

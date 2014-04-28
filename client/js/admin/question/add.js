c1b4654896ba3b16850aefb47255ed8a910f96f7
myounggun <myounggun@gmail.com>
2014-04-28 17:59:46 +0900

    #50 이미지 업로드 전 프리뷰 이미지 및 파일 정보 표시.

--- client/js/admin/question/add.js
+++ client/js/admin/question/add.js
@@ -1,28 +1,44 @@
 /*global, jquery $*/
 (function ($) {
-	/* 서버단에서 처리 
-	 var hours = [];
-	hours.push({name:"시간선택", value:""});
-	for (var i = 0; i < 24; i++) {
-		var value = i;
-		var name = (i % 12) ? i % 12 : 12;
-		var ampm = (i >= 12) ? 'PM' : 'AM';
-		
-		value = ('00' + value).slice(-2);
-		name  = ('00' + name).slice(-2);
-		
-		hours.push({
-				name  : name  + ':00 ' + ampm,
-				value : value + ':00'
-		});
-		
-		hours.push({
-			name  : name  + ':30 ' + ampm,
-			value : value + ':30'
-		});
-	}
 
-	$.each(hours, function(index, item) {
-        $('._hours').append('<option value="' + item.value + '">' + item.name + '</option>');
-    });*/
+    $('._image').change(onImageFileSelect);
+    
+    function onImageFileSelect(e) {
+        if (!window.File || !window.FileReader || !window.FileList) {
+            return;
+        }
+        
+        var target = $(e.currentTarget),
+            elImageHodler = target.parent(),
+            elImagePreview = elImageHodler.find('._imagePreview'),
+            elImageOutput  = elImageHodler.find('._imageOutput'),
+            files = target[0].files,
+            output = [];
+        
+        elImagePreview.empty();
+        for (var i = 0, f; f = files[i]; i++) {
+            if (!f.type.match('image.*')) {
+                output.push('<li><span style="color:#FF0000; font-face:Tahoma; font-size:8pt;">이미지 파일 포멧이 아닙니다.</span></li>');
+                continue;
+            }
+
+            output.push('<li><sapn style="font-face:Tahoma; font-size:8pt;">',
+                    '<strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                    f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                    '</span></li>');
+            
+            var reader = new FileReader();
+            
+            reader.onload = (function(f) {
+                return function(e) {
+                    elImagePreview.append('<img src="' + e.target.result + '" style="height: 75px;  border: 1px solid #000; margin: 10px 5px 0 0;"/>');
+                };
+            })(f);
+            
+            reader.readAsDataURL(f);
+        }
+        
+        elImageOutput.empty();
+        elImageOutput.append('<ul>' + output.join('') + '</ul>');
+    }
 }(jQuery));

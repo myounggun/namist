3f56acf33e2dd19fc2130b98349706d00485f055
myounggun <myounggun@gmail.com>
2014-04-29 17:17:22 +0900

    #53 문제 정보 조회 API 분리 및 리팩토링.

---
+++ server/apps/api/question/controllers/QuestionReadController.js
@@ -0,0 +1,26 @@
+/**
+ * 문제 정보 조회 API (query: id)
+ * /api/question/read?id=91
+ * 
+ */
+var Question = require('../../../admin/question/Question');
+
+module.exports = function(req, res) {
+    var query  = {id: req.query.id};
+    var fields = 'id date image time titles displayDate';
+    
+    Question.findOne(query, fields, function(err, doc) {
+        var status = 'error';
+
+        if (!err && doc) {
+            status = 'success';
+        }
+
+        var result = {
+            status : status,
+            item   : doc
+        };
+
+        res.json(result);
+    });
+}
--- server/apps/api/question/index.js
+++ server/apps/api/question/index.js
@@ -6,10 +6,10 @@ var app = module.exports = express();
 
 var controller = require('./controllers/QuestionController');
 var listController = require('./controllers/QuestionListController');
+var readController = require('./controllers/QuestionReadController');
 
 app.get('/api/question/list', listController);
-//app.get('/api/question/list', controller.list);
-app.get('/api/question/read', controller.read);
+app.get('/api/question/read', readController);
 app.get('/api/question/submit', controller.submit);
 app.get('/api/question/search', controller.search);
 app.get('/api/question/poll', controller.poll);
